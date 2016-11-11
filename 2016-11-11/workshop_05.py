928578169834392028b7370d1682b26b667d9979
VAle512 <valerio_cetorelli@yahoo.it>
2016-11-11 12:36:34 +0100

    add workshop_05.py. Not finished yet

---
+++ 2016-11-11/workshop_05.py
@@ -0,0 +1,139 @@
+'''workshop_05.py
+
+'''
+
+from larlib import *
+
+def ggpl_blackboard(dx,dy,dz):
+	'''creates a blackboard with given dimentions'''
+
+	#creates a whiteboard
+	blackboard = CUBOID([dx,dy,dz])
+	#creates the frame of the final structure
+	frame = OFFSET([.1,.1,.1])(SKEL_1(blackboard))
+	#paints the whiteboard black
+	blackboardC = COLOR(BLACK)(blackboard)
+
+	return(STRUCT([blackboardC,frame]))
+
+def ggpl_desk(dx,dy,dz):
+
+	#radius of a single leg
+	r = 0.05
+
+	#creates all four legs
+	leg = CYLINDER([r,dz])(100)
+	leg = COLOR(GRAY)(leg)
+	desk = leg
+	desk = STRUCT([desk,T([1,2,3])([dx,0,0]),leg])
+	desk = STRUCT([desk,T([1,2,3])([0,dy,0]),leg])
+	desk = STRUCT([desk,T([1,2,3])([dx,dy,0]),leg])
+
+	#creates the plane of the desk
+	plane = CUBOID([dx+(r*2),dy+(r*2),.075])
+	plane = COLOR(CYAN)(plane)
+	desk = STRUCT([desk,T([1,2,3])([-r,-r,dz]),plane])
+
+	#creates a second plane under the main one
+	underPlane = CUBOID([dx+(r*2),dy+(r*2),.05])
+	underPlane = COLOR(GRAY)(underPlane)
+	desk = STRUCT([desk,T([1,2,3])([-r,-r,(dz*5)/7]),underPlane])
+
+	return(desk)
+
+def ggpl_chair(dx,dy,dz):
+
+	#radius of a single leg
+	r = 0.025
+
+	#creates all four legs
+	legFront = CYLINDER([r,dz/2])(100)
+	legFront = COLOR(GRAY)(legFront)
+	legBack = CYLINDER([r,(dz*3)/4])(100)
+	legBack = COLOR(GRAY)(legBack)
+	chair = legBack
+	chair = STRUCT([chair,T([1,2,3])([dx,0,0]),legFront])
+	legBack = CYLINDER([r,(dz*3)/4])(100)
+	legBack = COLOR(GRAY)(legBack)
+	chair = STRUCT([chair,T([1,2,3])([0,dy,0]),legBack])
+	chair = STRUCT([chair,T([1,2,3])([dx,dy,0]),legFront])
+
+	#creates the sitting plane
+	sit = CUBOID([dx+(r*2),dy+(r*2),.05])
+	sit = COLOR(Color4f([205/255.0,170/255.0,125/255.0,1]))(sit)
+	chair = STRUCT([chair,T([1,2,3])([-r,-r,dz/3]),sit])
+
+	#creates the back of the chair
+	back = CUBOID([.05,dy+(r*2),dz/4])
+	back = COLOR(Color4f([205/255.0,170/255.0,125/255.0,1]))(back)
+	chair = STRUCT([chair,T([1,2,3])([-r,-r,(dz*3)/4]),back])
+
+	return(chair)
+
+def ggpl_desk_chair(dx,dy,dz):
+	desk = ggpl_desk(dx,dy,dz-.3)
+	desk = T(2)(0.2)(desk)
+	chair = ggpl_chair((dx/3),(dy/2),dz)
+	chair = R([1,2])(PI/2)(chair)
+	chair1 = T(1)(dx*3/4+.1)(chair)
+	chair2 = T(1)(dx*2/4-.1)(chair)
+	deskChair = STRUCT([desk,chair1,chair2])
+	
+	return(deskChair)
+
+def ggpl_classroom(dx,dy,dz):
+
+	#x,y,z of a single desk
+	deskX = 1.2
+	deskY = 0.7
+	deskZ = 0.9
+	
+	#desks distances
+	wallDist = 1.0
+	deskDist = 0.5
+
+	#x,y,z of the blackboard
+	blackboardX = 6
+	blackboardY = 2
+	blackboardZ = 0.1
+
+	#creates a single desk with two chairs
+	deskChair = ggpl_desk_chair(deskX,deskY,deskZ)
+	#insert the first desk and chairs inside the first row of the classroom
+	row = T(1)(wallDist)(deskChair)
+	#the counter is initialized at distance from the wall + length ad a desk + distance between desks
+	count = deskX + deskDist + wallDist
+	while dx - deskDist > count + deskX :
+		row = STRUCT([row,T(1)(count),deskChair])
+		count += deskDist + deskX
+
+	#insert the first row inside the classroom
+	room = T(2)(wallDist)(row)
+	count2 = deskDist + deskY + wallDist
+	while dy - deskDist > count2 + deskY :
+		room = STRUCT([room,T(2)(count2),row])
+		count2 += deskDist + deskY
+
+	#creates a box aroud the class
+	box = SKEL_1(CUBOID([dx,dy,dz]))
+
+	#creates the blackboard
+	blackboard = ggpl_blackboard(blackboardX,blackboardY,blackboardZ)
+	blackboard = R([2,3])(PI/2)(blackboard)
+	blackboard = T(1)((dx/2) - (blackboardX/2))(blackboard)
+	blackboard = T(2)(dy)(blackboard)
+	blackboard = T(3)(dz/4)(blackboard)
+
+	room = STRUCT([room,blackboard])
+
+	VIEW(STRUCT([room,box]))
+
+def main():
+	#ggpl_blackboard(2.5,1,.1)
+	#ggpl_desk(1.2,0.7,0.9)
+	#ggpl_chair(0.5,0.6,1.2)
+	#ggpl_desk_chair(1.2,0.7,0.9)
+	ggpl_classroom(20,10,3.5)
+
+if __name__ == '__main__':
+	main()
